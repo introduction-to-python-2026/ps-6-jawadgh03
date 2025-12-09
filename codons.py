3793f83a1442e219f6f95bb81096cc69ca741e27
jawadgh03 <jawadghaname12@gmail.com>
2025-12-09 19:55:52 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
 
+    codon_dict = {}
+    for row in rows:
+        parts = row.strip().split('\t')
+        codon = parts[0]
+        aa_abbrev = parts[2]
+        codon_dict[codon] = aa_abbrev
+
+    return codon_dict
 
